e7db7a97e01ab361f4e40de4408578e231954b76
ìJunhee <>
2021-10-28 18:13:43 +0000

    loss

--- train_hrnet.sh
+++ train_hrnet.sh
@@ -1,6 +1,7 @@
 python train.py \
-        --save_file_name hrnet_w48 \
+        --save_file_name hrnet_w48_focal.pt \
         --batch_size 16 \
-        --num_epochs 40 \
+        --num_epochs 60 \
         --model hrnet_w48 \
-        --learning_rate 1e-4
+        --learning_rate 1e-4 \
+        --criterion FocalLoss
