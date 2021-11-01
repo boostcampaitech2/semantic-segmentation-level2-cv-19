283461c6d2b9dd8b394095ba66e79eda5c35b052
ìJunhee <>
2021-11-01 07:55:09 +0000

    move folder

--- sh/train_hrnet.sh
+++ sh/train_hrnet.sh
@@ -1,7 +1,8 @@
 python train.py \
-        --save_file_name hrnet_w48_focal.pt \
+        --save_file_name hrnet_w48_focal2.pt \
         --batch_size 16 \
         --num_epochs 60 \
         --model hrnet_w48 \
-        --learning_rate 1e-4 \
+        --learning_rate 1e-3 \
         --criterion FocalLoss
+
