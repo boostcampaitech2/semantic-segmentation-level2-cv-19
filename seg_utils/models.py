dc4e14758dff5b8b377201fe35bd9ad11e7d41fe
ìJunhee <>
2021-11-04 16:42:42 +0000

    add Hrnet OCR

--- seg_utils/models.py
+++ seg_utils/models.py
@@ -3,12 +3,12 @@ import torch.nn as nn
 from torchvision import models
 from torch.nn import functional as F
 import segmentation_models_pytorch as smp
+# Hrnet OCR 
+import yaml 
+from hrnet_ocr.seg_hrnet_ocr import get_seg_model
 
-
-# ====================================================================================
 # ================================== fcn_resnet50 ====================================
-# ====================================================================================
-class fcn_resnet50(nn.Module):
+class FcnResnet50(nn.Module):
     '''
     fcn_resnet50
     '''
@@ -18,28 +18,24 @@ class fcn_resnet50(nn.Module):
         self.model.classifier[4] = nn.Conv2d(512, num_classes, kernel_size=1)
 
     def forward(self, x):
-        return self.model(x)
+        return self.model(x)['out']
 
-# ====================================================================================
 # ============================== efficientnet_b0_Unet ================================
-# ====================================================================================
-class efficientnet_b0(nn.Module):
+class EfficientnetB0(nn.Module):
     def __init__(self):
         super().__init__()
-        model = smp.Unet(encoder_name="efficientnet-b0", # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
-                        encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
-                        in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
-                        classes=11,)                    # model output channels (number of classes in your dataset)
+        model = smp.Unet(encoder_name="efficientnet-b0", 
+                        encoder_weights="imagenet",     
+                        in_channels=3,                  
+                        classes=11,)                    
 
     def forward(self, x):
         return self.model(x)
 
 
 
-# ====================================================================================
 # ================================== hrnet_unet ======================================
-# ====================================================================================
-class hrnet_w48(nn.Module):
+class HrnetW48(nn.Module):
     '''
     AI stage 토론 게시판
     https://stages.ai/competitions/78/discussion/talk/post/809
@@ -54,3 +50,62 @@ class hrnet_w48(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+
+# ================================== Unet++ efficientnet-b8 ======================================
+class UnetPlusPlusB8(nn.Module):
+    '''
+    AI stage 토론 게시판
+    https://stages.ai/competitions/78/discussion/talk/post/809
+    '''
+    def __init__(self, num_classes=11):
+        super().__init__()
+        self.model = smp.UnetPlusPlus(
+            encoder_name="timm-efficientnet-b8",
+            encoder_weights="imagenet",
+            in_channels=3,
+            classes=num_classes,
+            )
+    def forward(self, x):
+        return self.model(x)
+
+
+# ================================== Unet++ efficientnet-b7 ======================================
+class UnetPlusPlusB7(nn.Module):
+    '''
+    AI stage 토론 게시판
+    https://stages.ai/competitions/78/discussion/talk/post/809
+    '''
+    def __init__(self, num_classes=11):
+        super().__init__()
+        self.model = smp.UnetPlusPlus(
+            encoder_name="efficientnet-b7",
+            encoder_weights="imagenet",
+            in_channels=3,  
+            classes=11,  
+        )
+    def forward(self, x):
+        return self.model(x)
+
+
+# ================================== hrnet ocr ======================================
+# AssertionError: Default process group is not initialized
+# solution: https://github.com/pytorch/pytorch/issues/22538
+import torch.distributed as dist
+dist.init_process_group('gloo', init_method='file:///tmp/somefile', rank=0, world_size=1)
+    
+class HrnetOcr(nn.Module):
+    '''
+    https://stages.ai/competitions/78/discussion/talk/post/809 2.참고
+    '''
+    def __init__(self):
+        super().__init__()        
+        config_path='./hrnet_ocr/hrnet_seg.yaml'
+        with open(config_path) as f:
+            cfg = yaml.load(f)
+        self.encoder = get_seg_model(cfg)
+        
+    def forward(self, x):
+        x = self.encoder(x)
+        x = F.interpolate(input=x[0], size=(512, 512), mode='bilinear', align_corners=True)
+        return x
