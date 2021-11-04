99f8d11b5f7de3d35c0d701735bc1f51fad3ca4b
ìJunhee <>
2021-11-04 20:50:46 +0000

    additional requirements

---
+++ sh/install_requirements.sh
@@ -0,0 +1,6 @@
+# additional requirements in Segmentation server of AI stage
+apt update
+apt upgrade -y
+apt install gcc python3-dev python3-pip libxml2-dev libxslt1-dev zlib1g-dev g++ -y
+pip install git+https://github.com/lucasb-eyer/pydensecrf.git
+pip install easydict ttach
