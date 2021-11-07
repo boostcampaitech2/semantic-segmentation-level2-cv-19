ce75edf4ef9f7726fbdb9910398601e62d8426a8
ìJunhee <>
2021-11-07 04:26:49 +0000

    commit

--- sh/test_hrnet.sh
+++ sh/test_hrnet.sh
@@ -1,7 +1,7 @@
 python test.py \
         --save_file_name hrnet_w48_crf.csv \
         --model_path ./saved/hrnet_w48.pt \
-        --batch_size 4 \
+        --batch_size 16 \
         --model hrnet_w48 \
         --crf True
 
--- sh/train_hrnet.sh
+++ sh/train_hrnet.sh
@@ -1,7 +1,7 @@
 python train.py \
-        --save_file_name hrnet_w48_focal2.pt \
+        --save_file_name hrnet_w48.pt \
         --batch_size 16 \
-        --num_epochs 1 \
+        --num_epochs 20 \
         --model HrnetOcr \
         --learning_rate 1e-3 \
         --criterion DiceWCELoss
